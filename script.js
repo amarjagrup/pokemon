7eff021e77dfb1c4a1ba3b4c38c11d643e430cfc
amarjagrup <a30jagr@siena.edu>
2021-05-16 18:16:12 -0400

    Card flips when hovered

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//Thia script will display all 151 pokemon from the kanto region.
+
 const pokemonContainer = document.getElementById('pokedex');
 
 const getPokemon = async id => {
@@ -23,13 +25,27 @@ function displayPokemonCard(pokemon) {
 	let type = pokemon.types[0].type.name;
 	const name = pokemon.name[0];
 	
+	/*Thw const innerHtml will contain information about the front of the card and when hovered  
+	 * display different information about the pokemon.
+	 */
 	const innerHTML = `
-		         <li class="card">
-		        <img class="card-image" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${name}" "/>
-		        <h2 class="card_title">${pokemon.id}. ${pokemon.name}</h2>
-		        <p class="card_info">Type: ${type}</p>
-		         </li>`;
-
+		        
+				 <div class="flip-card">
+	<div class="flip-card-inner">
+	  <div class="flip-card-front">
+	  <li class="card">
+	  <img class="card-image" src="https://pokeres.bastionbot.org/images/pokemon/${pokemon.id}.png" alt="${name}" "/>
+	  <h2 class="card_title">${pokemon.id}. ${pokemon.name}</h2>
+	   </li>
+	  </div>
+	  <div class="flip-card-back">
+		<img class="card-image" src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/${pokemon.id}.png" alt="${name}" "/>
+		<p class= "info">Weight: ${pokemon.weight}</p>
+		<p class="info">Type: ${type}</p>
+	  </div>
+	</div>
+  </div>
+			`;
 	pokemonElement.innerHTML = innerHTML;
 
 	pokemonContainer.appendChild(pokemonElement);
